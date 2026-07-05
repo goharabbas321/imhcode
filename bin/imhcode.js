8a1b0ab8c060bee0a42ec1606d0365339a8c658e
Gohar Abbas <goharabbas321@gmail.com>
2026-07-05 13:46:06 +0300

    fix: resolve Process class import backslash escaping issue in DashboardController

--- bin/imhcode.js
+++ bin/imhcode.js
@@ -3107,7 +3107,7 @@ class Project extends Model {
 namespace App\\Http\\Controllers;
 
 use Illuminate\\Http\\Request;
-use Illuminate\\Support\Facades\\Process;
+use Illuminate\\Support\\Facades\\Process;
 use Illuminate\\Support\\Facades\\DB;
 
 class DashboardController extends Controller {
